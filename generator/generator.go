8d43336ac5ee5c4199699b80e1953a6f7cab9155
Maté Antunovic <mate AT schnooty.com>
2023-08-02 07:08:28 +1200

    refactor: remove some code and things

--- generator/generator.go
+++ generator/generator.go
@@ -16,330 +16,30 @@ type Generator struct {
 	Schema    map[string]*schema.Schema
 	Client    api.Client
 
-	ModifyRequestBody  func(interface{}) (interface{}, error)
-	ModifyResponseBody func(interface{}) (interface{}, error)
+	ModifyRequestBody  func(requestBody interface{}) (interface{}, error)
+	ModifyResponseBody func(responseBody interface{}) (interface{}, error)
 
 	ModifyRequest      func(url *string, headers *map[string]string, body *interface{}) error
 	ModifyResponse     func(headers *map[string]string, body *interface{}) error
-	ModifyResourceData func(*schema.ResourceData) error
-	GetId              func(*interface{}, *interface{}) string
+	ModifyResourceData func(resourceData *schema.ResourceData) error
+	GetId              func(requestBody *interface{}, responseBody *interface{}) string
 }
 
 func (generator *Generator) GenResource() schema.Resource {
 	create := func(d *schema.ResourceData, m interface{}) error {
-		// prepare request
-		api := m.(api.ProviderConfig).Api
-		requestVisitor := RequestVisitor{
-			schema: generator.Schema,
-		}
-
-		var path string
-		var err error
-		if generator.GetPath != nil {
-			path, err = generator.GetPath(d)
-			if err != nil {
-				return err
-			}
-		} else {
-			path = generator.Path
-		}
-
-		url, err := api.GetUrl(path)
-		if err != nil {
-			return err
-		}
-		accessToken, err := api.GetAccessToken()
-		if err != nil {
-			return err
-		}
-		headers := map[string]string{
-			"Authorization": "Bearer " + accessToken,
-		}
-
-		body, err := requestVisitor.accept(d)
-		if err != nil {
-			return err
-		}
-
-		// modify request
-
-		if generator.ModifyRequestBody != nil {
-			body, err = generator.ModifyRequestBody(body)
-			if err != nil {
-				return err
-			}
-		}
-
-		if generator.ModifyRequest != nil {
-			err = generator.ModifyRequest(&url, &headers, &body)
-			if err != nil {
-				return err
-			}
-		}
-
-		// send
-
-		response, err := generator.Client.Post(url, headers, body)
-		if err != nil {
-			return err
-		}
-
-		// modify response
-
-		if generator.ModifyResponseBody != nil {
-			response, err = generator.ModifyResponseBody(response)
-			if err != nil {
-				return err
-			}
-		}
-		if generator.ModifyResponse != nil {
-			generator.ModifyResponse(&map[string]string{}, &response) // TODO response headers
-		}
-
-		responseVisitor := ResponseVisitor{}
-		transformedResponse, err := responseVisitor.accept(response)
-		if err != nil {
-			return err
-		}
-
-		// consume response
-
-		var id string
-		if generator.GetId != nil {
-			id = generator.GetId(&body, &response)
-		} else {
-			id = responseVisitor.id
-		}
-
-		d.SetId(id)
-		if data, ok := transformedResponse.(map[string]interface{}); ok {
-			for key, val := range data {
-				err := d.Set(key, val)
-				if err != nil {
-					log.Printf("Unable to set '%s' = '%s'. Ignoring.", key, err)
-				}
-			}
-		}
-
-		return nil
+		return generator.sendRequestAndProcessResponse(d, m, "create")
 	}
 
 	read := func(d *schema.ResourceData, m interface{}) error {
-		api := m.(api.ProviderConfig).Api
-
-		var path string
-		var err error
-		if generator.GetPath != nil {
-			path, err = generator.GetPath(d)
-			if err != nil {
-				return err
-			}
-		} else {
-			path = generator.Path
-		}
-
-		url, err := api.GetUrl(path)
-		if err != nil {
-			return err
-		}
-		var fullUrl string
-		if generator.Singleton {
-			fullUrl = url
-		} else {
-			fullUrl = fmt.Sprintf("%s/%s", url, d.Id())
-		}
-		fmt.Printf("Using URL: %s", fullUrl)
-		accessToken, err := api.GetAccessToken()
-		if err != nil {
-			return err
-		}
-		headers := map[string]string{
-			"Authorization": "Bearer " + accessToken,
-		}
-
-		// send request
-
-		response, err := generator.Client.Get(fullUrl, headers)
-		if err != nil {
-			return err
-		}
-
-		// modify response
-
-		if generator.ModifyResponseBody != nil {
-			response, err = generator.ModifyResponseBody(response)
-			if err != nil {
-				return err
-			}
-		}
-		if generator.ModifyResponse != nil {
-			generator.ModifyResponse(&map[string]string{}, &response) // TODO response headers
-		}
-
-		// process response
-		responseVisitor := ResponseVisitor{}
-		transformedResponse, err := responseVisitor.accept(response)
-		if err != nil {
-			return err
-		}
-		if data, ok := transformedResponse.(map[string]interface{}); ok {
-			for key, val := range data {
-				err := d.Set(key, val)
-				if err != nil {
-					log.Printf("Unable to set '%s': '%s'. Ignoring.", key, err)
-				}
-			}
-		}
-
-		return nil
+		return generator.sendRequestAndProcessResponse(d, m, "read")
 	}
 
 	update := func(d *schema.ResourceData, m interface{}) error {
-		// prepare request
-
-		api := m.(api.ProviderConfig).Api
-		requestVisitor := RequestVisitor{
-			schema: generator.Schema,
-		}
-
-		var path string
-		var err error
-		if generator.GetPath != nil {
-			path, err = generator.GetPath(d)
-			if err != nil {
-				return err
-			}
-		} else {
-			path = generator.Path
-		}
-
-		url, err := api.GetUrl(path)
-		if err != nil {
-			return err
-		}
-		var fullUrl string
-		if generator.Singleton {
-			fullUrl = url
-		} else {
-			fullUrl = fmt.Sprintf("%s/%s", url, d.Id())
-		}
-		accessToken, err := api.GetAccessToken()
-		if err != nil {
-			return err
-		}
-		headers := map[string]string{
-			"Authorization": "Bearer " + accessToken,
-		}
-
-		body, err := requestVisitor.accept(d)
-		if err != nil {
-			return err
-		}
-
-		// modify request
-
-		if generator.ModifyRequestBody != nil {
-			body, err = generator.ModifyRequestBody(body)
-			if err != nil {
-				return err
-			}
-		}
-
-		if generator.ModifyRequest != nil {
-			err = generator.ModifyRequest(&url, &headers, &body)
-			if err != nil {
-				return err
-			}
-		}
-
-		// send
-
-		response, err := generator.Client.Put(fullUrl, headers, body)
-		if err != nil {
-			return err
-		}
-
-		// modify response
-
-		if generator.ModifyResponseBody != nil {
-			response, err = generator.ModifyResponseBody(response)
-			if err != nil {
-				return err
-			}
-		}
-		if generator.ModifyResponse != nil {
-			generator.ModifyResponse(&map[string]string{}, &response) // TODO response headers
-		}
-
-		// process response
-
-		responseVisitor := ResponseVisitor{}
-		transformedResponse, err := responseVisitor.accept(response)
-		if err != nil {
-			return err
-		}
-
-		var id string
-		if generator.GetId != nil {
-			id = generator.GetId(&body, &response)
-		} else {
-			id = responseVisitor.id
-		}
-
-		d.SetId(id)
-		if data, ok := transformedResponse.(map[string]interface{}); ok {
-			for key, val := range data {
-				err := d.Set(key, val)
-				if err != nil {
-					log.Printf("Unable to set'%s': '%s'. Ignoring.", key, err)
-				}
-			}
-		}
-
-		return nil
+		return generator.sendRequestAndProcessResponse(d, m, "update")
 	}
 
 	deleteResource := func(d *schema.ResourceData, m interface{}) error {
-		// prepare request
-
-		api := m.(api.ProviderConfig).Api
-
-		var path string
-		var err error
-		if generator.GetPath != nil {
-			path, err = generator.GetPath(d)
-			if err != nil {
-				return err
-			}
-		} else {
-			path = generator.Path
-		}
-
-		url, err := api.GetUrl(path)
-		if err != nil {
-			return err
-		}
-		var fullUrl string
-		if generator.Singleton {
-			fullUrl = url
-		} else {
-			fullUrl = fmt.Sprintf("%s/%s", url, d.Id())
-		}
-		accessToken, err := api.GetAccessToken()
-		if err != nil {
-			return err
-		}
-		headers := map[string]string{
-			"Authorization": "Bearer " + accessToken,
-		}
-
-		// send
-
-		err = generator.Client.Delete(fullUrl, headers)
-
-		// consume response
-
-		return err
+		return generator.sendRequestAndProcessResponse(d, m, "delete")
 	}
 
 	resource := schema.Resource{
@@ -354,4 +54,126 @@ func (generator *Generator) GenResource() schema.Resource {
 	}
 
 	return resource
+}
+
+// Helper function for sending requests and processing responses
+func (generator *Generator) sendRequestAndProcessResponse(d *schema.ResourceData, m interface{}, method string) error {
+	api := m.(api.ProviderConfig).Api
+	requestVisitor := RequestVisitor{
+		schema: generator.Schema,
+	}
+
+	var path string
+	var err error
+	if generator.GetPath != nil {
+		path, err = generator.GetPath(d)
+		if err != nil {
+			return err
+		}
+	} else {
+		path = generator.Path
+	}
+
+	log.Printf("Path is %s", path)
+
+	url, err := api.GetUrl(path)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Url is '%s'", url)
+
+	fullUrl := url
+	if !generator.Singleton && (method == "read" || method == "update") {
+		fullUrl = fmt.Sprintf("%s/%s", url, d.Id())
+	}
+
+	accessToken, err := api.GetAccessToken()
+	if err != nil {
+		return err
+	}
+	headers := map[string]string{
+		"Authorization": "Bearer " + accessToken,
+	}
+
+	var body interface{}
+	if method != "read" {
+		body, err = requestVisitor.accept(d)
+		if err != nil {
+			return err
+		}
+	}
+
+	// modify request
+	if generator.ModifyRequestBody != nil {
+		body, err = generator.ModifyRequestBody(body)
+		if err != nil {
+			return err
+		}
+	}
+
+	if generator.ModifyRequest != nil {
+		err = generator.ModifyRequest(&url, &headers, &body)
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Printf("Full URL '%s'", fullUrl)
+
+	// send request
+	var response interface{}
+	switch method {
+	case "create":
+		response, err = generator.Client.Post(fullUrl, headers, body)
+	case "read":
+		response, err = generator.Client.Get(fullUrl, headers)
+	case "update":
+		response, err = generator.Client.Put(fullUrl, headers, body)
+	case "delete":
+		err = generator.Client.Delete(fullUrl, headers)
+	default:
+		return fmt.Errorf("unknown method: %s", method)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	// modify response
+	if generator.ModifyResponseBody != nil {
+		response, err = generator.ModifyResponseBody(response)
+		if err != nil {
+			return err
+		}
+	}
+	if generator.ModifyResponse != nil {
+		generator.ModifyResponse(&map[string]string{}, &response) // TODO response headers
+	}
+
+	// process response
+	responseVisitor := ResponseVisitor{}
+	transformedResponse, err := responseVisitor.accept(response)
+	if err != nil {
+		return err
+	}
+
+	var id string
+	if generator.GetId != nil {
+		id = generator.GetId(&body, &response)
+	} else {
+		id = responseVisitor.id
+	}
+
+	d.SetId(id)
+	if data, ok := transformedResponse.(map[string]interface{}); ok {
+		for key, val := range data {
+			err := d.Set(key, val)
+			if err != nil {
+				log.Printf("Unable to set '%s' = '%s'. Ignoring.", key, err)
+			}
+		}
+	}
+
+	return nil
 }
--- provider/resource_verifier.go
+++ provider/resource_verifier.go
@@ -48,4 +48,3 @@ func resourceVerifier() *schema.Resource {
 	provider := verifierGenerator.GenResource()
 	return &provider
 }
-
